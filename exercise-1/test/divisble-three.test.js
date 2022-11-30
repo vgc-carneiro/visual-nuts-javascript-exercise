f4fa49bad5be4437c4586304192aa738a4ece35a
Vinicius Carneiro <vinicius.carneiro@tmov.com.br>
2022-11-29 22:06:08 -0300

    Including functionallity for divisible by 5 and 3

---
+++ exercise-1/test/divisble-three.test.js
@@ -0,0 +1,29 @@
+const {
+  integerDivisibleByThree,
+} = require('../src/useCase/integerDivisibleByThree');
+
+describe('Check integer is divisible per 3', () => {
+  test('Check if null is not divisible per 3', () => {
+    expect(integerDivisibleByThree(null)).toBeFalsy();
+  });
+
+  test('Check if 0 is not divisible per 3', () => {
+    expect(integerDivisibleByThree(0)).toBeFalsy();
+  });
+
+  test('Check if 1 is not divisible per 3', () => {
+    expect(integerDivisibleByThree(1)).toBeFalsy();
+  });
+
+  test('Check if 3 is divisible per 3', () => {
+    expect(integerDivisibleByThree(3)).toBeTruthy();
+  });
+
+  test('Check if 5 is not divisible per 3', () => {
+    expect(integerDivisibleByThree(5)).toBeFalsy();
+  });
+
+  test('Check if 6 is not divisible per 3', () => {
+    expect(integerDivisibleByThree(6)).toBeTruthy();
+  });
+});
